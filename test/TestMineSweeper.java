aa59e3b34dcf377746284d9ad9626e06cb3a514d
Saaanzio <rafaelsanziosilva22@gmail.com>
2024-04-28 23:03:43 -0300

    Initial commit

---
+++ test/TestMineSweeper.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class TestMineSweeper {
+
+    @Test
+    public void test(){
+        int a = 1 + 1;
+        assertEquals(2,a);
+    }
+}
